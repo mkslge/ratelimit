6a41a30cdfa1999e1ba241f049b98dd69cd4c2dc
mkslge <marklseeliger@gmail.com>
2026-03-25 20:01:34 -0400

    completed basic version of fixed window

---
+++ limiters/LeakyBucket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mark on 3/25/26.
+//
+
+#include "LeakyBucket.h"
---
+++ limiters/LeakyBucket.h
@@ -0,0 +1,16 @@
+//
+// Created by Mark on 3/25/26.
+//
+
+#ifndef LEAKYBUCKET_H
+#define LEAKYBUCKET_H
+
+
+
+class LeakyBucket {
+
+};
+
+
+
+#endif //LEAKYBUCKET_H
